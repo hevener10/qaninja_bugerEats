ecbf541bccac7b997fbb121df4a5e5d6d5223aa7
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2021-12-20 21:05:09 -0300

    Aula 4 Preenchendo o endereço

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -17,13 +17,26 @@ describe("Cadastro", () => {
       cpf: "00000014141",
       email: "email@email.com",
       whatsapp: "11999999999",
-      cep: "01001000",
+
+      endereco: {
+        cep: "75860000",
+        rua: "Rua dos tapajos",
+        numero: "100",
+        complemento: "casa",
+        bairro: "Centro",
+        cidade_uf: "Quirinópolis/GO",
+      },
     };
 
     cy.get('input[name="name"]').type(entregador.nome);
     cy.get('input[name="cpf"]').type(entregador.cpf);
     cy.get('input[name="email"]').type(entregador.email);
     cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
-    cy.get('input[name="postalcode"]').type(entregador.cep);
+    cy.get('input[name="postalcode"]').type(entregador.endereco.cep);
+    cy.get('input[type="button"][value="Buscar CEP"]').click();
+    cy.get('input[name="address-number"]').type(entregador.endereco.numero);
+    cy.get('input[name="address-details"]').type(
+      entregador.endereco.complemento
+    );
   });
 });
